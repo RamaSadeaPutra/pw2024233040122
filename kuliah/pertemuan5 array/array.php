dd11f6dc85547389eb77ba4766ef4c99e93ae111
Rama <ramaamon7796@gmail.com>
2024-03-07 17:49:13 +0700

    mengerjakan tugas 4

---
+++ kuliah/pertemuan5 array/array.php
@@ -0,0 +1,24 @@
+<?php 
+$hari1 = 'senin';
+$hari2 = 'selasa';
+$hari3 = 'rabu';
+$hari4 = 'kamis';
+$hari5 = 'jumat';
+$hari6 = 'sabtu';
+$hari7 = 'minggu';
+$pembangkit='jagung';
+$harii = array ('909090','selasa','rabu','kamis','jumat','sabtu','minggu');
+$hari = ['senin','selasa','rabu','kamis','jumat','sabtu','minggu'];
+$kereta =['lokomotif',300.0,'gerbong barang',[['penumpang1','aa'],'penumpang2'],$pembangkit,1];
+var_dump($kereta[2]);
+var_dump($kereta[3] [0][0]);
+
+// var_dump($hari [5]);
+
+// echo ($hari[6]),"<br>";
+
+// echo '<br>';
+
+// var_dump($harii);
+
+?>
